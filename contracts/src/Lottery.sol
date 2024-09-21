3f886be1166c617b10de5cd732638f4e6ed9943b
0xJordan <me@0xjordan.dev>
2024-09-21 14:50:39 +0800

    feat: contract update

--- contracts/src/Lottery.sol
+++ contracts/src/Lottery.sol
@@ -14,15 +14,23 @@ contract Lottery is Ownable {
     uint256 public totalWinners;
 
     mapping(address => uint256) public players;
+    mapping(address => bool) public played;
     mapping(address => uint256) public winners;
+
+    address[] public playersArray;
+    address[] public winnersArray;
 
     event LotteryEntered(address indexed player, uint256 amount);
     event LotteryEnded(address indexed winner, uint256 prize);
     event RewardClaimed(address indexed player, uint256 amount);
 
-    constructor(string _name, uint256 _duration, uint256 _startAt, uint256 _totalWinners, IYieldStrategy _yieldStrategy)
-        Ownable()
-    {
+    constructor(
+        string memory _name,
+        uint256 _duration,
+        uint256 _startAt,
+        uint256 _totalWinners,
+        IYieldStrategy _yieldStrategy
+    ) Ownable() {
         name = _name;
         duration = _duration;
         startAt = _startAt;
@@ -33,48 +41,47 @@ contract Lottery is Ownable {
     function enterLottery(uint256 amount) external payable {
         require(amount > 0, "Amount must be greater than 0");
         require(block.timestamp < startAt + duration, "Lottery is closed");
+        require(!played[msg.sender], "You have already played");
 
         yieldStrategy.deposit(amount);
         totalAmount += amount;
         totalPlayers += 1;
         players[msg.sender] += amount;
+        played[msg.sender] = true;
 
         emit LotteryEntered(msg.sender, amount);
     }
 
     function endLottery() public onlyOwner {
         require(block.timestamp >= startAt + duration, "Lottery is still open");
+        require(played[msg.sender], "You have not played");
 
         uint256 prize = yieldStrategy.estimateYield();
         yieldStrategy.withdraw(prize);
         totalWinners += 1;
 
-        emit LotteryEnded(winner, prize);
+        emit LotteryEnded(msg.sender, prize);
     }
 
     function claimReward() external {}
 
     function getPlayers() public view returns (address[] memory) {
         address[] memory _players = new address[](totalPlayers);
-        uint256 index = 0;
+
         for (uint256 i = 0; i < totalPlayers; i++) {
-            if (players[i] > 0) {
-                _players[index] = i;
-                index += 1;
-            }
+            _players[i] = playersArray[i];
         }
+
         return _players;
     }
 
-    function getWinners() public view returns (address) {
+    function getWinners() public view returns (address[] memory) {
         address[] memory _winners = new address[](totalWinners);
-        uint256 index = 0;
+
         for (uint256 i = 0; i < totalWinners; i++) {
-            if (winners[i] > 0) {
-                _winners[index] = i;
-                index += 1;
-            }
+            _winners[i] = winnersArray[i];
         }
+
         return _winners;
     }
 
