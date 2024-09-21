656bf09bfb3a08132882659600ab738baa987312
Iceywil <scherrer.wilfried@gmail.com>
2024-09-22 01:51:23 +0800

    sad

--- web/src/app/landing.jsx
+++ web/src/app/landing.jsx
@@ -2,43 +2,13 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from 'next/image'
+import Navbar from "./components/navbar"
 
 export default function Component() {
   return (
     <div className="min-h-screen bg-[#FCF7E9] flex flex-col">
       <div className="w-full border-b-[2px] border-[#D42A20]">
-        <header className="container mx-auto px-4 py-4 flex items-center justify-between">
-          <div className="flex-1 flex justify-start">
-            <Input
-              className="max-w-xs bg-[#D9D9D9] border-none text-center"
-              placeholder="Search projects, creators, categories"
-              style={{ borderRadius: '20px' }}
-            />
-          </div>
-          <div className="flex-1 flex justify-center">
-            <Image
-              src="/handsup.png"
-              alt="handsUp Logo"
-              width={80}
-              height={20}
-              className="header-logo"
-            />
-          </div>
-          <nav className="flex-1 hidden md:flex items-center justify-end space-x-10">
-            <Link className="text-gray-600 hover:text-gray-900" href="#">
-              How it's work
-            </Link>
-            <Link className="text-gray-600 hover:text-gray-900" href="#">
-              About
-            </Link>
-            <Link className="text-gray-600 hover:text-gray-900" href="#">
-              Login
-            </Link>
-            <Link className="text-gray-600 hover:text-gray-900" href="#">
-              Register
-            </Link>
-          </nav>
-        </header>
+        <Navbar/>
       </div>
       <main className="flex-grow flex flex-col items-center justify-center px-4 mt-[50px]">
         <div className="text-center mb-20">
